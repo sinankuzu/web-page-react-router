fce3cdfbbbd4aeda062e27e53247361ceea53192
Sinan <sinankzu@gmail.com>
2022-10-30 19:37:21 +0100

    aa

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App.css";
-import {Link} from "react-router-dom"
 
 const Footer = () => {
   return (
@@ -8,14 +7,14 @@ const Footer = () => {
       <footer>
         <p>Clarusway Web Design, Copyright &copy; 2020</p>
         <br />
-        <Link to={{ pathname:"http://www.clarusway.com"}} target="_blank">
+        <a href="http://www.clarusway.com" target="_blank">
           <img
             src="https://clarusway.com/wp-content/uploads/2020/06/clarusway_logo.png"
             width="200"
             height="40"
             alt=""
           />
-        </Link>
+        </a>
       </footer>
     </div>
   );
