86f648d7323a0777f89fc8a15d4aa6ac12092454
Sinan <sinankzu@gmail.com>
2022-10-30 19:24:49 +0100

    aa

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <footer>
         <p>Clarusway Web Design, Copyright &copy; 2020</p>
         <br />
-        <Link to="http://www.clarusway.com" target="_blank">
+        <Link to={{ pathname:"http://www.clarusway.com"}} target="_blank">
           <img
             src="https://clarusway.com/wp-content/uploads/2020/06/clarusway_logo.png"
             width="200"
